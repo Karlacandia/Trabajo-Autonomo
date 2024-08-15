00292a64e61365d11fdacd0cd8056626412344b1
Karlacandia <Karlacandia009@gmail.com>
2024-08-15 10:12:34 -0500

    Actividades de esta semana en clase

---
+++ Trabajo.js
@@ -0,0 +1,15 @@
+// Imprimir números del 1 al 10
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// Contar cuántos números son mayores que 5
+let contadorMayoresQue5 = 0;
+for (let i = 1; i <= 10; i++) {
+    if (i > 5) {
+        contadorMayoresQue5++;
+    }
+}
+
+// Imprimir el resultado
+console.log(`Hay ${contadorMayoresQue5} números mayores que 5.`);
